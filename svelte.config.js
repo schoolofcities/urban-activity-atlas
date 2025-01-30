a79417b83329c992f79df573e64c8486bfa64816
Jeff Allen <jeffallen89@gmail.com>
2025-01-30 07:46:37 -0500

    config updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import preprocess from 'svelte-preprocess';
+
+const dev = "production" === "development";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,12 +12,27 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+	// 	adapter: adapter()
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// adapter: adapter()
+		
+		adapter: adapter({
+		    pages: "docs",
+		    assets: "docs"
+		}),
+		paths: {
+		    // change below to your repo name
+		    base: dev ? "" : "/urban-activity-atlas",
+		}
+		}
 };
 
 export default config;
