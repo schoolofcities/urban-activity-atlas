cd190dbf861c73bea5d8de15c68a05ceddc56a25
an-k45 <an_k27@protonmail.com>
2025-02-11 12:34:19 -0500

    Refactor out dropdown component

--- src/lib/SidePanel.svelte
+++ src/lib/SidePanel.svelte
@@ -1,5 +1,6 @@
 <script>
     import logo from '../assets/top-logo-full.svg';
+    import Dropdown from './Dropdown.svelte';
 
     // Props
     export let metroRegionCentroids;
@@ -10,31 +11,14 @@
     export let selectLocation;
     export let mapDimensionView;
 
-    
     // Sort the features alphabetically by name
     metroRegionCentroids.features.sort((a, b) =>
         a.properties.name.localeCompare(b.properties.name)
     );
 
-    let filteredOptions = metroRegionCentroids.features;
-
-    // Filter the options based on the search query
-    $: filteredOptions = metroRegionCentroids.features.filter((feature) =>
-        feature.properties.name.toLowerCase().startsWith(searchQuery.toLowerCase())
-    );
-
-    function handleKeydown(e) {
-        if (e.key === 'Enter' && filteredOptions.length > 0) {
-            e.preventDefault();
-            selectLocation(filteredOptions[0].properties.name);
-            dropdownOpen = false;
-        }
-    }
-
     function setMapDimensionView(dimension) {
         mapDimensionView = dimension;
     }
-
 </script>
 
 <div>
@@ -53,46 +37,14 @@
     </p> 
 
     <!-- <p class="location-label">Select a metropolitan region:</p> -->
-    <div class="dropdown-container">
-        <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <div
-            class="dropdown-toggle"
-            role="button"
-            tabindex="0"
-            aria-haspopup="listbox"
-            aria-expanded={dropdownOpen}
-            on:click={() => { if (!searchQuery) dropdownOpen = true; }}
-        >
-            <input
-                type="text"
-                class="search-input"
-                bind:value={searchQuery}
-                placeholder={searchQuery ? '' : 'Search metropolitan region...'}
-                on:input={handleInputChange}
-                on:click={handleSearchInputClick}
-                on:keydown={handleKeydown}
-            />
-        </div>
-    
-        {#if dropdownOpen}
-            <div class="dropdown-list" role="listbox">
-                {#each searchQuery === '' ? metroRegionCentroids.features : filteredOptions as feature}
-                    <!-- svelte-ignore a11y_click_events_have_key_events -->
-                    <div
-                        class="dropdown-item"
-                        role="option"
-                        aria-selected="false"
-                        tabindex="0"
-                        on:click={() => selectLocation(feature.properties.name)}
-                    >
-                        {feature.properties.name}
-                    </div>
-                {:else}
-                    <div class="no-results">No results found</div>
-                {/each}
-            </div>
-        {/if}
-    </div>
+    <Dropdown
+        {metroRegionCentroids}
+        bind:searchQuery
+        bind:dropdownOpen
+        {selectLocation}
+        {handleInputChange}
+        {handleSearchInputClick}
+    />
 
     <p class="description">
         The part of the grid with a
@@ -204,60 +156,6 @@
         color: white;
         font-family: RobotoRegular;
         font-size: 1rem;
-    }
-
-    .dropdown-container {
-        position: relative;
-        margin: 0 15px 0 15px;
-    }
-
-    .dropdown-toggle {
-        width: 100%;
-        cursor: pointer;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .search-input {
-        width: 100%;
-        margin-top: 5px;
-        margin-bottom: 5px;
-        height: 25px;
-        border: solid 1px var(--brandGray);
-        border-radius: 5px;
-        font-family: RobotoRegular;
-        background-color: black;
-        color: white;
-        padding-left: 5px;
-    }
-
-    .dropdown-list {
-        position: absolute;
-        top: 40px;
-        left: 0;
-        right: 0;
-        background-color: black;
-        max-height: 200px;
-        overflow-y: auto;
-        z-index: 10;
-    }
-
-    .dropdown-item {
-        padding: 3px;
-        font-family: RobotoRegular;
-        font-size: .8rem;
-        cursor: pointer;
-        color: white;
-    }
-
-    .dropdown-item:hover {
-        background-color: var(--brandMedBlue);
-    }
-
-    .no-results {
-        padding: 8px;
-        color: gray;
     }
 
     .legend {
